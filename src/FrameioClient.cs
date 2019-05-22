c4f297e4458a6c1cdbcf004f093a1896bc795209
Matt Dwen <matt@dwen.co.nz>
2019-05-22 19:42:35 +1200

    feat: create FrameioClient class

---
+++ src/FrameioClient.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace FrameioNet
+{
+    public class FrameioClient
+    {
+        #region Constructors
+
+        /// <summary>
+        /// Initialize client with a developer token
+        /// <param name="developerToken">Developer token</param>
+        /// </summary>
+        public FrameioClient(string developerToken) {
+            SetDeveloperToken(developerToken);
+        }
+
+        #endregion Constructors
+
+        #region Declarations
+
+        private static string _developerToken;
+
+        #endregion Declarations 
+
+        #region Private Methods
+
+        /// <summary>
+        /// Set the developer token
+        /// <param name="developerToken">Developer token</param>
+        /// </summary>
+        private static void SetDeveloperToken(string developerToken) {
+            if (string.IsNullOrWhiteSpace(developerToken)) {
+                throw new ArgumentException("Developer token cannot be empty or null");
+            }
+
+            _developerToken = developerToken;
+        }
+
+        #endregion Private Methods
+    }
+}
