ac26f0da121b3252082384dbc7ea08cb642e29cf
Tyrone Smith <dev@sleuth.co.nz>
2019-05-31 22:44:02 +1200

    Add interface summaries

--- src/FrameIoNet/Frameio.NET/Interfaces/IFrameIoClient.cs
+++ src/FrameIoNet/Frameio.NET/Interfaces/IFrameIoClient.cs
@@ -2,13 +2,26 @@
 {
     interface IFrameIoClient
     {
+        /// <summary>
+        /// Assets <see cref="IAssets"/>
+        /// </summary>
         IAssets Assets { get; }
 
+        /// <summary>
+        /// Projects <see cref="IProjects"/>
+        /// </summary>
         IProjects Projects { get; }
 
+        /// <summary>
+        /// Teams <see cref="ITeams"/>
+        /// </summary>
         ITeams Teams { get; }
 
+        /// <summary>
+        /// Users <see cref="IUsers"/>
+        /// </summary>
         IUsers Users { get; }
+
     }
 
 }
--- src/FrameIoNet/Frameio.NET/Interfaces/IUsers.cs
+++ src/FrameIoNet/Frameio.NET/Interfaces/IUsers.cs
@@ -4,6 +4,11 @@ using Frameio.NET.Models;
 namespace Frameio.NET.Interfaces {
     public interface IUsers
     {
+
+        /// <summary>
+        /// Returns detail about the current user
+        /// </summary>
+        /// <returns></returns>
         Task<User> GetCurrentUser();
     }
 }
