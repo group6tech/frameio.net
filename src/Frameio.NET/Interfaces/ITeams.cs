a98d04b33310eecc28490257e8ff8af926900f52
Matt Dwen <matt@dwen.co.nz>
2019-10-09 12:33:31 +1300

    Fix ITeams interface

--- src/Frameio.NET/Interfaces/ITeams.cs
+++ src/Frameio.NET/Interfaces/ITeams.cs
@@ -6,6 +6,14 @@ namespace Frameio.NET.Interfaces
     public interface ITeams
     {
         /// <summary>
+        /// Returns a list list of Teams for the current user
+        /// </summary>
+        /// <param name="pageSize"></param>
+        /// <param name="page"></param>
+        /// <returns></returns>
+        Task<PagedResult<Team>> GetTeams(int pageSize = 10, int page = 1);
+
+        /// <summary>
         /// Returns a list list of Teams for the given accountId
         /// </summary>
         /// <param name="accountId"></param>
